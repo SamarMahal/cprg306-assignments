e03ee6780433840f788b614fc8639255eefe85fe
SamarMahal <141462664+SamarMahal@users.noreply.github.com>
2024-02-12 23:42:38 -0700

    week-4

--- app/page.js
+++ app/page.js
@@ -1,6 +1,15 @@
 import Image from "next/image";
 
 export default function Home() {
+
+  return (
+    <div>
+      <a href="/week-2">Week 2</a>
+      <a href="/week-3">Week 3</a>
+      <a href="/week-4">Week 4</a> 
+    </div>
+  );
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
